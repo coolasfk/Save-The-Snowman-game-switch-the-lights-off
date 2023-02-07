38fa67042ae46da59e250713d1a0ff8bcfb3e43a
coolasfk <fk.przybyla@gmail.com>
2023-02-07 16:44:20 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const playBtn = document.querySelector(".play-btn-wrapper__play-btn");
+
+// console.log(playBtn);
+
+// console.log(window.location.href);
+
+// playBtn.addEventListener("click", () => {
+//  );
+//   window.location.href = "game.html";
+// });
